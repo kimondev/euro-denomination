692cd9efd6aa3987438925b55b715f8cf70d3681
rachid <rachid.azzaui@stud.h-da.de>
2026-03-28 12:18:40 +0100

    initialize Spring boot project

---
+++ euro-denomination-backend/src/main/java/kimondev/euro_denomination_backend/EuroDenominationBackendApplication.java
@@ -0,0 +1,13 @@
+package kimondev.euro_denomination_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuroDenominationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuroDenominationBackendApplication.class, args);
+	}
+
+}
---
+++ euro-denomination-backend/src/test/java/kimondev/euro_denomination_backend/EuroDenominationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kimondev.euro_denomination_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuroDenominationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
