db7077603f1ddaee4709d85342f4e47e85a924b5
rachid <rachid.azzaui@stud.h-da.de>
2026-03-28 14:47:56 +0100

    Implement calculate method in DenominationService

--- euro-denomination-backend/src/main/java/kimondev/euro_denomination_backend/service/DenominationService.java
+++ euro-denomination-backend/src/main/java/kimondev/euro_denomination_backend/service/DenominationService.java
@@ -1,10 +1,25 @@
 package kimondev.euro_denomination_backend.service;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class DenominationService {
+
     public Map<Integer, Integer> calculate(int amount) {
         System.out.println("Calculating denomination for amount: " + amount);
-        return null;
+
+        int[] denominations = {500, 200, 100, 50, 20, 10, 5, 2, 1};
+
+        Map<Integer, Integer> result = new HashMap<>();
+
+        for (int denom : denominations) {
+            if (amount >= denom) {
+                int count = amount / denom;
+                result.put(denom, count);
+                amount = amount % denom;
+            }
+        }
+
+        return result;
     }
 }
--- euro-denomination-backend/src/test/java/kimondev/euro_denomination_backend/service/DenominationServiceTest.java
+++ euro-denomination-backend/src/test/java/kimondev/euro_denomination_backend/service/DenominationServiceTest.java
@@ -7,15 +7,15 @@ import java.util.Map;
 
 public class DenominationServiceTest {
     @Test
-    void shouldCalculateDenominationFor137Euros() {
+    void shouldCalculateDenominationFor158Euros() {
         DenominationService service = new DenominationService();
 
-        Map<Integer, Integer> result = service.calculate(137);
+        Map<Integer, Integer> result = service.calculate(158);
 
         assertThat(result).containsEntry(100, 1);
-        assertThat(result).containsEntry(20, 1);
-        assertThat(result).containsEntry(10, 1);
+        assertThat(result).containsEntry(50, 1);
         assertThat(result).containsEntry(5, 1);
         assertThat(result).containsEntry(2, 1);
+        assertThat(result).containsEntry(1, 1);
     }
 }
