f68ac300dcee80ac355cfcaeb7f34723a8461a9e
rachid <rachid.azzaui@stud.h-da.de>
2026-03-29 11:25:46 +0200

    Adjust PrimeNG configurations in app.config.ts

--- euro-denomination-frontend/src/app/app.config.ts
+++ euro-denomination-frontend/src/app/app.config.ts
@@ -1,12 +1,25 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import Nora from "@primeuix/themes/nora";
 
 import { routes } from './app.routes';
+import {providePrimeNG} from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+
+    providePrimeNG({
+      theme: {
+        preset: Nora,
+        options: {
+          darkModeSelector: ".dark-mode",
+        },
+      },
+    }),
   ]
 };
